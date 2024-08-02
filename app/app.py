2b1e213c831eac91bc018bd5b245668a6eb0bf72
ankitkumarc <98842479+ankitkumarc@users.noreply.github.com>
2024-08-02 23:21:21 +0530

    auth added

---
+++ app/app.py
@@ -0,0 +1,59 @@
+from beanie import init_beanie
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+import uvicorn
+from app.models.retailer_model import User
+from app.models.customer_model import Customer
+from app.core.config import settings
+from app.api.api_v1.router import router 
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask, send_from_directory
+import os
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app2 = Flask(__name__, static_folder='build')
+
+# Serve React App
+@app2.route('/', defaults={'path': ''})
+@app2.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app2.static_folder + '/' + path):
+        return send_from_directory(app2.static_folder, path)
+    else:
+        return send_from_directory(app2.static_folder, 'index.html')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5000","http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def app_init():
+    """
+    Initialize crucial application services
+    """
+    
+    db_client = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING, tls=True, tlsAllowInvalidCertificates=True).retailsense
+    await init_beanie(
+        database=db_client,
+        document_models=[
+            Customer,
+            User,
+        ]
+    )
+
+app.include_router(router=router,prefix="/api/v1")
+app.mount("/", WSGIMiddleware(app2))
+
+
+
+
---
+++ app/services/customer_service.py
@@ -0,0 +1,76 @@
+import statistics
+from typing import List
+from uuid import UUID
+from datetime import datetime
+from fastapi import HTTPException, status
+from app.models.retailer_model import User
+from app.models.customer_model import Customer
+from app.schemas.customer_schema import CustomerCreate, CustomerUpdate,CustomerOut
+
+class CustomerService:
+    @staticmethod
+    async def list_customers()->List[CustomerOut]:
+        customers = await Customer.find_all().to_list()
+        return customers
+    
+    @staticmethod
+    async def create_customer( data: CustomerCreate) -> Customer:
+        existing_customer = await Customer.find_one(Customer.phone_number==data.phone_number)
+        if existing_customer:
+            raise HTTPException(status_code=statistics.HTTP_, detail="Customer with this phone already exists")
+        
+        data_dict = data.dict()
+        data_dict.setdefault("visit_frequency", 1)
+        customer = Customer(**data.dict())
+        return await customer.insert()
+    
+    @staticmethod
+
+    async def retrieve_customer(customer_id: UUID):
+        customer = await Customer.find_one(Customer.customer_id == customer_id)
+        if customer:
+            return customer
+
+        raise HTTPException(status_code=statistics.HTTP_404_NOT_FOUND, detail="Customer not found")
+
+    @staticmethod
+    async def retrieve_customer2(phone_number: str):
+        customer = await Customer.find_one(Customer.phone_number == phone_number)
+        if customer:
+            return customer
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+
+    
+    @staticmethod
+    async def update_customer(customer_id: UUID, data: CustomerUpdate):
+        customer = await CustomerService.retrieve_customer(customer_id)
+        if customer:
+            if(data.total_bill_amount > 0):
+                data.visit_frequency = customer.visit_frequency + 1
+                
+            data.total_bill_amount += customer.total_bill_amount
+            
+            await customer.update({"$set": data.dict(exclude_unset=True)})
+            await customer.save()
+            return customer
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+
+    
+    @staticmethod
+    async def delete_customer(customer_id:UUID):
+        customer = await CustomerService.retrieve_customer( customer_id)
+        if customer:
+            await customer.delete()
+            return ValueError("Customer is deleted")
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+    
+    # @staticmethod
+    # async def customer_purchase_year(year: int, customer_id) -> List[Customer]:
+    #     # Retrieve customers whose purchase_date is in the specified year
+    #     start_date = datetime(year, 1, 1)
+    #     end_date = datetime(year, 12, 31)
+    #     purchases = await PurchaseService.list_purchases()
+    #     customers = await Customer.find(Customer.purchase_date >= start_date, Customer.purchase_date <= end_date).to_list()
+    #     return customers
